7486c1628ec5ba9f99c4bfd4cefd1087e0f6a1b9
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-03-21 22:23:16 +0530

    Add files via upload

---
+++ local and global functions.js
@@ -0,0 +1,13 @@
+var name="Sajan";
+
+function myExample()
+{
+    name1="Manikandan";
+}
+function myExample1()
+{
+    console.log("Welcome "+name+" and "+name1);
+}
+
+myExample();
+myExample1();
