35f18c8cb031dfed6668406228cd7b25446dc469
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-03-21 22:06:05 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,19 @@
+var myArr=["Happy","sad","excitement"];
+myArr.shift();
+console.log(myArr);
+myArr.unshift("Angry");
+console.log(myArr);
+
+//functions
+function functionExample()
+{
+    console.log("Hello world!");
+}
+functionExample();
+
+//functions with parameter
+function functionExample1(a,b)
+{
+    console.log(a+b);
+}
+functionExample1(5,6);
