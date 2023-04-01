0dfd6eb4cf8617508ee87ca5ba5c06f65648b0e5
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-04-01 21:50:51 +0530

    Add files via upload

---
+++ random function.js
@@ -0,0 +1,12 @@
+function randomFunction()
+{
+    return Math.random();
+}
+console.log(randomFunction());
+
+function random()
+{
+    return Math.floor(Math.random()*10);
+
+}
+console.log(random());
